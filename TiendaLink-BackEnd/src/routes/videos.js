d8f7653fdaf3e1e3607f16d57e056a09c2ab90eb
FlushaM <83796763+FlushaM@users.noreply.github.com>
2025-11-05 01:22:09 -0300

    corregierondo , errores

--- TiendaLink-BackEnd/src/routes/videos.js
+++ TiendaLink-BackEnd/src/routes/videos.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const videosController = require('../controllers/videosController');
-const { authMiddleware } = require('../middlewares/authMiddleware');
-const { verifyAdmin } = require('../middlewares/verifyRole');
 
 router.get('/', videosController.getAllVideos);
 router.get('/section/:sectionId', videosController.getVideosBySection);
-router.post('/', authMiddleware, verifyAdmin, videosController.createVideo);
-router.put('/:id', authMiddleware, verifyAdmin, videosController.updateVideo);
-router.delete('/:id', authMiddleware, verifyAdmin, videosController.deleteVideo);
+router.post('/', videosController.createVideo);
+router.put('/:id', videosController.updateVideo);
+router.delete('/:id', videosController.deleteVideo);
 
 module.exports = router;
